dfa92a4ae0df2628e794862e185ff1179f1b9a20
NezukoRaven <carlosespinoza.huenuante@gmail.com>
2025-04-04 00:12:24 -0300

    Change background color of NexusBall component from white to black

--- src/pages/NexusBall.tsx
+++ src/pages/NexusBall.tsx
@@ -148,7 +148,7 @@ const NexusBall: React.FC<NexusBallProps> = ({ onBack }) => {
   };
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden bg-white">
+    <div className="relative w-screen h-screen overflow-hidden bg-black">
       <canvas
         ref={canvasRef}
         className="absolute top-0 left-0 w-full h-full"
